a77ed606bf72a6db8488423f6f4618d6196e74a3
gustav018 <gustavofleitas18@gmail.com>
2024-05-02 20:24:04 -0400

    Actualizacion para unir ramas

--- server/server.js
+++ server/server.js
@@ -57,9 +57,9 @@ const getUsers = async () => {
 
 // Programar tarea para enviar correos cada x tiempo 
 //(por ejemplo, cada minuto ='* * * * *' )
-const dia = 29; // Día del mes en que deseas ejecutar la tarea
-const hora = '22'; // Hora en formato de 24 horas
-const minuto = '27'; // Minuto en que deseas ejecutar la tarea
+const dia = 2; // Día del mes en que deseas ejecutar la tarea
+const hora = '20'; // Hora en formato de 24 horas
+const minuto = '09'; // Minuto en que deseas ejecutar la tarea
 ////`${minuto} ${hora} ${dia} * *`
 cron.schedule(`${minuto} ${hora} ${dia} * *`, async () => {
   const users = await getUsers();
